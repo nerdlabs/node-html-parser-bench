0aee02f98d217ea1dc3a2788d40269042c8e2d50
ZauberNerd <zaubernerd@zaubernerd.de>
2014-10-05 13:26:30 +0200

    added more parsers

--- benchmark.js
+++ benchmark.js
@@ -4,7 +4,13 @@ var fs = require('fs'),
     htmlparser = require("htmlparser"),
     htmlparser2 = require('htmlparser2'),
     html5 = require('html5'),
-    parse5 = require('parse5');
+    parse5 = require('parse5'),
+    gumbo = require("gumbo-parser"),
+    High5 = require('high5'),
+    htmlParser = require("html-parser"),
+    hubbub = require('hubbub'),
+    sax = require("sax"),
+    FastHTMLParser = require('fast-html-parser');
 
 var dataDirPath = path.join(__dirname, './data'),
     testPages = fs.readdirSync(dataDirPath).map(function (fileName) {
@@ -44,6 +50,50 @@ new Benchmark.Suite()
         for (var i = 0; i < testPages.length; i++) {
             var parser = new parse5.Parser();
             parser.parse(testPages[i]);
+        }
+    })
+
+    .add('gumbo-parser (https://github.com/karlwestin/node-gumbo-parser)', function () {
+        for (var i = 0; i < testPages.length; i++) {
+            gumbo(testPages[i]);
+        }
+    })
+
+    .add('high5 (https://github.com/fb55/high5)', function () {
+        for (var i = 0; i < testPages.length; i++) {
+            var parser = new High5();
+            parser.end(testPages[i]);
+        }
+    })
+
+    .add('html-parser (https://github.com/tmont/html-parser)', function () {
+        for (var i = 0; i < testPages.length; i++) {
+            htmlParser.parse(testPages[i], {});
+        }
+    })
+
+    .add('hubbub (https://github.com/deanmao/node-hubbub)', function () {
+        for (var i = 0; i < testPages.length; i++) {
+            var handler = new hubbub.DefaultHandler();
+            var parser = new hubbub.Parser(handler);
+            parser.parseComplete(testPages[i]);
+        }
+    })
+
+    .add('sax (https://github.com/isaacs/sax-js)', function () {
+        for (var i = 0; i < testPages.length; i++) {
+            var parser = sax.parser(false);
+            var noop = function () {};
+            parser.onend = noop;
+            parser.onerror = noop;
+            parser.write(testPages[i]);
+            parser.close();
+        }
+    })
+
+    .add('fast-html-parser (https://github.com/ashi009/node-fast-html-parser)', function () {
+        for (var i = 0; i < testPages.length; i++) {
+            FastHTMLParser.parse(testPages[i]);
         }
     })
 
